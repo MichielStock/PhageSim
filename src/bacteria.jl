5e8d875e3ca17249b6675cc0f06293b523e76d73
michielstock <michielfmstock@gmail.com>
2020-07-10 19:41:27 +0200

    :wrench: bacteria with energy :zap:

--- src/bacteria.jl
+++ src/bacteria.jl
@@ -9,9 +9,9 @@ Implementation of the bacteria using Agents.jl
 =#
 
 export AbstractBacterium, Bacterium
-export bacteria, prophage, haslatent, species, prophage!, density
-export AbstractBacteriaRules, BacteriaRules, ProphageBacteriaRules,
-            HeteroBacteriaRules, agent_step!
+export bacteria, prophage, haslatent, species, prophage!, density, energy, energy!
+export AbstractBacteriaRules, BacteriaRules
+export AbstractEnergyupdate, ConstantEnergyUpdate, RandomEnergyUpdate
 
 # BACTERIA TYPE AND FUNCTIONS
 # ---------------------------
@@ -22,13 +22,29 @@ mutable struct Bacterium <: AbstractBacterium
     id::Int
     pos::Tuple{Int,Int}
     species::Int  # decribes the species of the bacterium
+    energy::Float64
     prophage::Union{Int,Nothing}  # either carries a latent phage (i) or not (0)
 end
 
-Bacterium(id, pos, species=1) = Bacterium(id, pos, species, nothing)
+Bacterium(id, pos, species=1, energy=1.0) = Bacterium(id, pos, species, energy, nothing)
 
 """Test if agent is a bacterium."""
 bacteria(a::AbstractAgent) = a isa AbstractBacterium
+
+"""
+    energy(bact::AbstractBacterium)
+
+Get the energy level of a bacterium.
+"""
+energy(bact::AbstractBacterium) = bact.energy
+
+"""
+    energy!(bact::AbstractBacterium, level::Float64)
+
+Set the energy level of a bacterium.
+"""
+energy!(bact::AbstractBacterium, level::Float64) = (bact.energy = level)
+
 
 """
     prophage(bact::AbstractBacterium)
@@ -76,9 +92,9 @@ If `species=false`, all bacteria will be taken into account. The default behavio
 takes bacteria of the same species as `bact`.
 """
 function density(bact, model, R, forspecies::Bool=true)
-    #FIXME : does not work properly
     bactinregion = space_neighbors(bact.pos, model, R)
-    nbact = forspecies ? count(b->species(b, species(bact)), bactinregion) : length(bactinregion)
+	nbact = count(a->bacteria(a) && (!forspecies || species(a)==species(bact)),
+							model.agents[bactinregion])
     return nbact / (R + 1)^2
 end
 
@@ -86,87 +102,49 @@ end
 # RULES AND BEHAVIOUR
 # -------------------
 
+abstract type AbstractEnergyupdate end
+
+struct ConstantEnergyUpdate <: AbstractEnergyupdate
+	ΔE::Float64
+	Emax::Float64
+end
+
+struct RandomEnergyUpdate <: AbstractEnergyupdate
+	ΔE::Float64
+	Emax::Float64
+	σE::Float64
+end
+
+function energyupdate(bact::AbstractBacterium, ur::ConstantEnergyUpdate)
+	bact.energy += ur.ΔE
+	bact.energy > ur.Emax && energy!(bact, ur.Emax)
+end
+
+function energyupdate(bact::AbstractBacterium, ur::RandomEnergyUpdate)
+	bact.energy += ur.ΔE + ur.σE * randn()
+	bact.energy > ur.Emax && energy!(bact, ur.Emax)
+end
+
 abstract type AbstractBacteriaRules end
 
-"""
-Rules for when all bacteria behave the same.
-"""
-struct BacteriaRules <: AbstractBacteriaRules
-    prepr::Float64
-    pmove::Float64
-    pdie::Float64
-    function BacteriaRules(prepr::Float64, pmove::Float64, pdie::Float64)
-        @assert +(prepr, pmove, pdie) ≤ 1.0 &&
-                all((prepr, pmove, pdie) .≥ 0) "behaviour of the bacteria should be valid probabilites"
-        new(prepr, pmove, pdie)
-    end
+struct BacteriaRules{TPM,TED,TUF<:AbstractEnergyupdate} <: AbstractBacteriaRules
+    pmove::TPM
+	Ediv::TED
+	Eupdate::TUF
 end
 
-"""
-Rules for when species of bacteria might show different behaviours.
-"""
-struct HeteroBacteriaRules <: AbstractBacteriaRules
-    prepr::Array{Float64,1}
-    pmove::Array{Float64,1}
-    pdie::Array{Float64,1}
-end
+br(bactrules::BacteriaRules) = bactrules
 
-function BacteriaRules(prepr::Array{Float64,1}, pmove::Array{Float64,1}, pdie::Array{Float64,1})
-    @assert all(.+(prepr, pmove, pdie) .≤ 1.0) && all(prepr .≥ 0) && all(pmove .≥ 0) &&
-             all(pdie .≥ 0) "behaviour of the bacteria should be valid probabilites"
-    HeteroBacteriaRules(prepr, pmove, pdie)
-end
+#TODO write constructor for this stuff
 
-"""
-Rules for when a bacterium with a prophage exhibits a different behaviour from
-a bacterium without a prophage.
-"""
-struct ProphageBacteriaRules <: AbstractBacteriaRules
-    probsnoinf::Tuple{Float64,Float64,Float64}
-    probsinf::Tuple{Float64,Float64,Float64}
-    precover::Float64
-end
+getspeciespar(bact::AbstractBacterium, par::Number) = par
+getspeciespar(bact::AbstractBacterium, par::AbstractVector) = par[species(bact)]
 
-function BacteriaRules(probsnoinf::Tuple{Float64,Float64,Float64},
-                probsinf::Tuple{Float64,Float64,Float64},
-                precover::Float64=0.0)
-    @assert sum(probsnoinf) ≤ 1.0 &&
-            all(probsnoinf .≥ 0) "behaviour of the bacteria should be valid probabilites"
-    @assert sum(probsinf) ≤ 1.0 &&
-            all(precover .≥ 0) "behaviour of the bacteria should be valid probabilites"
-    @assert 0 ≤ precover ≤ 1 "`precover` should be a valid probability"
-    ProphageBacteriaRules(probsnoinf, probsinf, precover)
-end
-
-"""
-    bacteriaprobs(br::BacteriaRules, bact::AbstractBacterium)
-
-Get the behaviour parameters for a specific bacterium.
-"""
-bacteriaprobs(bact::AbstractBacterium, br::BacteriaRules) = (br.prepr, br.pmove, br.pdie)
-
-"""
-    bacteriaprobs(br::ProphageBacteriaRules, bact::AbstractBacterium)
-
-Get the behaviour parameters depending on whether the bacterium has a prophage
-or not.
-"""
-bacteriaprobs(bact::AbstractBacterium, br::HeteroBacteriaRules) =
-                    species(bact) |> i -> (br.prepr[i], br.pmove[i], br.pdie[i])
-
-bacteriaprobs(bact::AbstractBacterium, br::ProphageBacteriaRules) =
-        haslatent(bact) ? br.probsinf : br.probsinf
-
-"""
-By default an infected phage cannot recover.
-"""
-recovers(bact::AbstractBacterium, br::AbstractBacteriaRules) = false
-
-"""
-Computes the probability that a bacterium with a prophage looses recovers.
-"""
-recovers(bact::AbstractBacterium, br::ProphageBacteriaRules) = br.precover > 0.0 && rand() < br.precover
-
+pmove(bact, br) = getspeciespar(bact, br.pmove)
+Emax(bact, br) = getspeciespar(bact, br.Emax)
+Ediv(bact, br) = getspeciespar(bact, br.Ediv)
+ΔEmain(bact, br) = getspeciespar(bact, br.ΔEmain)
+Eupdate(br) = br.Eupdate
 
 """
     agent_step!(bact::Bacterium, model)
@@ -177,41 +155,35 @@ function agent_step!(bact::AbstractBacterium, model)
     bactrules = br(model.properties)  # get the bacteria rules
     # if the bacterium has a prophage it might lyse or recover
     if haslatent(bact)
-        if recovers(bact, bactrules)
-            bact.prophage = nothing  # cured
-        else
-            interactionrules = ir(model.properties)
-            lyses(bact, interactionrules, model) && return lyse!(bact, model)
-        end
+        interactionrules = ir(model.properties)
+        lyses(bact, interactionrules, model) && return lyse!(bact, model)
     end
-    # get behaviour parameters
-    prepr, pmove, pdie = bacteriaprobs(bact, bactrules)
-    r = rand()
-    if r < pmove
-        return move!(bact, model)
-    elseif r < pmove + prepr
-        return reproduce!(bact, model)
-    elseif r < pmove + prepr + pdie
-        return kill_agent!(bact, model)
-    end
+	# update energy
+	energyupdate(bact, Eupdate(bactrules))
+	energy(bact) < 0.0 && return kill_agent!(bact, model)
+	# try to move
+    pmove(bact, bactrules) ≤ rand() && move!(bact, model)
+    (energy(bact) > Ediv(bact, bactrules)) && reproduce!(bact, model)
 end
 
 """Check if there are no bacteria on a node."""
-isfree(node, model) = !any(bacteria, get_node_agents(node, model))
+isfree(pos, model) = !any(bacteria, get_node_agents(pos, model))
 
 function move!(bact::AbstractBacterium, model)
     neighbors = node_neighbors(bact, model)
-    node = rand(neighbors)
-    if isfree(node, model)
-        move_agent!(bact, node, model)
+    pos = rand(neighbors)
+    if isfree(pos, model)
+        move_agent!(bact, pos, model)
     end
 end
 
 function reproduce!(bact::AbstractBacterium, model)
     neighbors = node_neighbors(bact, model)
-    node = rand(neighbors)
-    if isfree(node, model)
+    pos = rand(neighbors)
+    if isfree(pos, model)
+		E = energy(bact) / 2
+		energy!(bact, E)
         id = nextid(model)
-        add_agent!(Bacterium(id, node, bact.species, bact.prophage), model)
+        add_agent!(Bacterium(id, pos, bact.species, E, bact.prophage), pos, model)
     end
 end
