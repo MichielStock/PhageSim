18402abb164ffe507f16976937b17154f4a565bb
michielstock <michielfmstock@gmail.com>
2021-01-26 10:13:14 +0100

    🦄 experiments interaction types

---
+++ scripts/interactions/analyse.jl
@@ -0,0 +1,50 @@
+#=
+Created on Friday 16 October 2020
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Assessing the effect of the structure of the interaction matrix on the bacteria-phage matrix.
+File to plot the results.
+=#
+
+using DrWatson
+quickactivate(@__DIR__, "PhageSim")
+using PhageSim
+using Statistics
+using Plots, StatsPlots
+using Agents, AgentsPlots
+using LinearAlgebra
+using CSV, DataFrames
+
+for infecttype in ["uniform", "unique", "nested", "secundair"]
+    results = CSV.read(datadir("interactions/$infecttype.csv"), DataFrame)
+
+    #@df results corrplot([:count_bacteria1, :count_bacteria2, :count_bacteria3, :count_phages1, :count_phages2, :count_phages3])
+    corrplot(results[:,5:end-1] |> Matrix, labels=names(results)[5:end-1])
+    savefig(plotsdir("interactions/$(infecttype)_corplot.png"))
+
+    # plot growth curves
+    for rep in 1:50
+       
+        results_rep = results[results.replicate.==rep,:]
+
+        pbact = plot(results_rep.count_bacteria1, label="bacteria sp. 1", color=:red)
+        plot!(results_rep.count_bacteria2, label="bacteria sp. 2", color=:blue)
+        plot!(results_rep.count_bacteria3, label="bacteria sp. 3", color=:green)
+        xlabel!("time")
+        title!("Bacteria")
+
+        pphage = plot(results_rep.count_phages1, label="phages sp. 1", color=:red, ls=:dash)
+        plot!(results_rep.count_phages2, label="phages sp. 2", color=:blue, ls=:dash)
+        plot!(results_rep.count_phages3, label="phages sp. 3", color=:green, ls=:dash)
+        xlabel!("time")
+        title!("Phages")
+
+        plot(pbact, pphage, layout=(2, 1))
+
+        savefig(plotsdir("interactions/growth_curves/growth_$(infecttype)_$rep.pdf"))
+    end
+end
+
---
+++ scripts/interactions/simulate.jl
@@ -0,0 +1,84 @@
+#=
+Created on Friday 09 October 2020
+Last update: Tuesday 26 January 2021
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Assessing the effect of the structure of the interaction matrix on the bacteria-phage matrix.
+=#
+
+using DrWatson
+quickactivate(@__DIR__, "PhageSim")
+using PhageSim, Plots
+using Agents, AgentsPlots
+using LinearAlgebra
+using BSON, CSV
+
+repl = 50
+tsteps = 1000
+
+dims = (50, 50)
+
+# structures of matrix
+p = 0.1
+
+Punif = p
+Punique = 3p * Matrix(I, 3, 3)
+Pnested = [p/3 0   0;
+           p/3 p/2 0;
+           p/3 p/2 p]
+Psec = [p   0   p/2;
+        p/2 p   0;
+        0   p/2 p]
+
+# general parameters
+burstsize = 5.0
+pdecay = 0.1
+plysogeny = 0.0
+plysis = 0.05
+pmovebact = 0.5
+ΔE = 2.0
+Ediv = 10.0
+Emax = 25.0
+σE = 2.0
+
+n_bacteria = 500
+n_phages = 10_000
+
+parameters = Dict(
+    :burstsize => 1.0,
+    :pdecay => 0.1,
+    :plysogeny => false,
+    :plysis => 0.0,
+    :pmovebact => 0.5,
+    :ΔE => 2.0,
+    :Ediv => 10.0,
+)
+
+adata = [(bacteria, count), (phages, count), (haslatent, count),
+        (bacteria1, count), (bacteria2, count), (bacteria3, count),
+        (phages1, count), (phages2, count), (phages3, count)]
+
+for (infecttype, Pinf) in zip(["uniform", "unique", "nested", "secundair"],
+                        [Punif, Punique, Pnested, Psec])
+
+    println("Simulating $infecttype...")
+
+    # generate basic rules
+    rules = PhageSimRules(Pinf=Pinf, burstsize=burstsize, pdecay=pdecay, plysogeny=plysogeny,
+                    plysis=plysis, pmovebact=pmovebact, Ediv=Ediv,
+                    Eupdate=RandomEnergyUpdate(ΔE, Emax, σE))
+    
+    model = initialize_model(rules,
+                    n_bacteria=n_bacteria,
+                    n_phages=n_phages,
+                    n_phage_sp=3,
+                    n_bact_sp=3,
+                    dims=dims)
+
+
+    results, _ = run!(model, agent_step!, tsteps, adata=adata, replicates=repl, parallel=true)
+    safesave(datadir("interactions/$infecttype.csv"), results)
+
+end
